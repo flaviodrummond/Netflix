252b8a12af19e7fb0a80a8ed7bcbab81d2fcc13e
flaviodrummond <flaviosax20@gmail.com>
2024-10-21 21:55:06 -0300

    Retirada do botão de tocar o som, adicionado o audio de abertura da netflix e atualizações no JavaScript para o som começar crescentemente

--- scripts.js
+++ scripts.js
@@ -2,20 +2,24 @@ let buttonSound = document.querySelector(".sound")
 let video = document.querySelector(".main-video")
 let info = document.querySelector(".information")
 let infoBox = document.querySelector(".box")
+let audio = document.querySelector(".audio-opening")
 
-
-buttonSound.addEventListener("click", clickSom)
 info.addEventListener("click", showInformation)
 infoBox.addEventListener("click", hideInformation)
 
+window.addEventListener("load", playAudio)
+
+function playAudio() {
+    audio.play()
+}
+
+window.addEventListener("load", clickSom)
 
 function clickSom() {
 
-    if (video.muted) {
-        video.muted = false
-    } else {
-        video.muted = true
-    }
+   video.play()
+
+   fadeInAudio(video, 5);
 
 }
 
@@ -27,5 +31,19 @@ function hideInformation() {
     infoBox.style.display = "none"
 }
 
+function fadeInAudio(videoElement, duration) {
+    const audioContext = new (window.AudioContext || window.webkitAudioContext)();
+    const track = audioContext.createMediaElementSource(videoElement);
+    const gainNode = audioContext.createGain();
+    
+    // Conecta o áudio do vídeo ao gainNode
+    track.connect(gainNode).connect(audioContext.destination);
+
+    // Define o volume inicial do vídeo como 0 (silencioso)
+    gainNode.gain.setValueAtTime(0, audioContext.currentTime);
+    
+    // Aumenta o volume até 1 (volume máximo) durante a duração especificada
+    gainNode.gain.linearRampToValueAtTime(1, audioContext.currentTime + duration);
+}
 
 
